d45d9ee707252cf105cbe81ef7b8d63ac06d0d5d
umeshkanala22 <umesh.cse.iitd@gmail.com>
2024-09-01 13:05:03 +0530

    inititalized the files

---
+++ A1/solution.py
@@ -0,0 +1,20 @@
+class Agent(object):
+    def __init__(self, phoneme_table, vocabulary) -> None:
+        """
+        Your agent initialization goes here. You can also add code but don't remove the existing code.
+        """
+        self.phoneme_table = phoneme_table
+        self.vocabulary = vocabulary
+        self.best_state = None
+
+    def asr_corrector(self, environment):
+        """
+        Your ASR corrector agent goes here. Environment object has following important members.
+        - environment.init_state: Initial state of the environment. This is the text that needs to be corrected.
+        - environment.compute_cost: A cost function that takes a text and returns a cost. E.g., environment.compute_cost("hello") -> 0.5
+
+        Your agent must update environment.best_state with the corrected text discovered so far.
+        """
+        self.best_state = environment.init_state
+        cost = environment.compute_cost(environment.init_state)
+
---
+++ README.md
@@ -0,0 +1,1 @@
+run driver.py to run the code and output.json contains output of the solution.py by correcting the sentence
