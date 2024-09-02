d61d8689950450c83d1534e7b263a1da9667e577
umeshkanala22 <umesh.cse.iitd@gmail.com>
2024-09-02 11:35:44 +0530

    completed beam search

--- A1/solution.py
+++ A1/solution.py
@@ -18,61 +18,71 @@
 #         self.best_state = environment.init_state
 #         cost = environment.compute_cost(environment.init_state)
 
+import heapq
+
+class Node(object):
+    def __init__(self, string, cost):
+        self.string = string
+        self.cost = cost
+
+    def __lt__(self, other):
+        return self.cost < other.cost
+
 class Agent(object):
     def __init__(self, phoneme_table, vocabulary) -> None:
         self.phoneme_table = phoneme_table
         self.vocabulary = vocabulary
         self.best_state = None
+        self.k = 10
+        self.heap = []
 
     def asr_corrector(self, environment):
         self.best_state = environment.init_state
         cost = environment.compute_cost(environment.init_state)
+        node = Node(environment.init_state, cost)
+        self.heap_push(node)
+        self.search(environment)
+        environment.best_state = self.best_state.string
 
-        # Implement search method to find best correction
-        self.search(environment, environment.init_state, cost)
+    def heap_push(self, node):
+        if len(self.heap) < self.k:
+            heapq.heappush(self.heap, node)
+        else:
+            heapq.heappushpop(self.heap, node)
 
-        # Update best state with the corrected text
-        environment.best_state = self.best_state
+    def search(self, environment):
+        while self.heap:
+            node = heapq.heappop(self.heap)
+            currstring=node.string
+            for i in range(len(currstring)):
+                if currstring[i] in self.phoneme_table:
+                    for phoneme in self.phoneme_table[currstring[i]]:
+                        new_string = currstring[:i] + phoneme + currstring[i+1:]
+                        new_cost = environment.compute_cost(new_string)
+                        if(new_cost<node.cost):
+                            new_node = Node(new_string,new_cost)
+                            self.heap_push(new_node)
+                
 
-    def search(self, environment, state, cost):
-        # Simple beam search implementation
-        beam_size = 10
-        beam = [(state, cost)]
+            self.best_state = heapq.top().string
+        self.heap=[heapq.top()]
+        heapq.clear()
+        while self.heap:
+            node = heapq.heappop(self.heap)
+            currstring=node.string
+            for i in range(-1,self.vocabulary.size):
+                for j in range(i-1,self.vocabulary.size):
+                    if(i!=-1):
+                        new_string=self.vocabulary[i]+currstring
+                    if(j!=i-1):
+                        new_string=currstring+self.vocabulary[j]
+                    new_cost = environment.compute_cost(new_string)
+                    if(new_cost<node.cost):
+                        new_node = Node(new_string,new_cost)
+                        self.heap_push(new_node)
+            self.best_state = heapq.top().string
 
-        for _ in range(10):  # Number of iterations
-            new_beam = []
-            for state, cost in beam:
-                for word in state.split():
-                    if word not in self.vocabulary:
-                        # Generate corrections for the word
-                        corrections = self.generate_corrections(word)
-                        for correction in corrections:
-                            new_state = state.replace(word, correction)
-                            new_cost = environment.compute_cost(new_state)
-                            new_beam.append((new_state, new_cost))
-
-            # Select the top beam_size states with the lowest cost
-            beam = sorted(new_beam, key=lambda x: x[1])[:beam_size]
-
-        # Update best state with the corrected text
-        self.best_state = beam[0][0]
-
-    def generate_corrections(self, word):
-        # Simple correction generation using phoneme table
-        corrections = []
-        for vocab_word in self.vocabulary:
-            if self.phoneme_distance(word, vocab_word) <= 2:
-                corrections.append(vocab_word)
-        return corrections
-
-    def phoneme_distance(self, word1, word2):
-        # Implement phoneme distance calculation using phoneme table
-        phonemes1 = self.phoneme_table[word1]
-        phonemes2 = self.phoneme_table[word2]
-        distance = 0
-        for i in range(min(len(phonemes1), len(phonemes2))):
-            if phonemes1[i] != phonemes2[i]:
-                distance += 1
-        distance += abs(len(phonemes1) - len(phonemes2))
-        return distance
-
+            
+            # Add new nodes to the heap
+            # ...
+            # self.heap_push(new_node)
