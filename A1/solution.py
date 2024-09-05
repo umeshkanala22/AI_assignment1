0c0d4ddff4585364df138fda631d353040dbdfe0
umeshkanala22 <umesh.cse.iitd@gmail.com>
2024-09-05 16:31:13 +0530

    lucky code

--- A1/solution.py
+++ A1/solution.py
@@ -1,71 +1,129 @@
-class Node(object):
-    def __init__(self, string, cost):
-        self.string = string
-        self.cost = cost
-
-    def __lt__(self, other):
-        return self.cost < other.cost
-
 class Agent(object):
     def __init__(self, phoneme_table, vocabulary) -> None:
-        self.phoneme_table = {}
-        for key, values in phoneme_table.items():
-            for value in values:
-                if value not in self.phoneme_table:
-                    self.phoneme_table[value] = [key]
-                else:
-                    self.phoneme_table[value].append(key)
+        self.phoneme_table = phoneme_table
         self.vocabulary = vocabulary
         self.best_state = None
-        self.node = None
 
     def asr_corrector(self, environment):
         self.best_state = environment.init_state
         cost = environment.compute_cost(environment.init_state)
-        self.node = Node(environment.init_state, cost)
-        print(self.best_state)
-        self.search(environment)
 
+        vocab = self.vocabulary
+        phoneme = self.phoneme_table
+        s = environment.init_state
+        c = environment.compute_cost(s)
+        sentence = s
+        cost = c
 
+        transition = {}
+        for key in phoneme:
+            for v in phoneme[key]:
+                if v not in transition:
+                    transition[v] = []
+                transition[v].append(key)
 
-    def search(self, environment):
-        isend=False
-        while isend==False:
-            node =self.node
-            refstring=node.string
-            print(self.best_state)
-          
-            currstring=node.string
-            for i in range(0,len(currstring)):
-                for key in self.phoneme_table:
-                    if currstring[i:i+len(key)] == key:
-                        for phoneme in self.phoneme_table[key]:
-                            new_string = currstring[:i] + phoneme + currstring[i+len(key):]
-                            new_cost = environment.compute_cost(new_string)
-                            if(new_cost<self.node.cost):
-                                self.best_state=new_string
-                                print("my cost is low",new_string)
-                                self.node.string=new_string
-                                self.node.cost=new_cost
-                             
-                
+        n = len(s)
+        subs = 0
+        for i in range(n - 2):
+            if (s[i] in transition):
+                subs += 1
+            elif (s[i:i + 2] in transition):
+                subs += 1
+            elif (s[i:i + 3] in transition):
+                subs += 1
+
+        if (subs == 0):
+            max_dep = 100
+        else:
+            max_dep = 1000 // subs
             
-            if(refstring==self.node.string):
+        max_dep = max(max_dep, 10)
+        
+        dp = [False] * n
+        dep = 0
+        while (True):
+            dep += 1
+            if (dep > max_dep):
                 break
-    
-        for i in range(0,len(self.vocabulary)):
-            new_string = self.vocabulary[i] +" "+ self.node.string
-            new_cost = environment.compute_cost(new_string)
-            if(self.node.cost>new_cost):
-                self.node.cost=new_cost
-                self.best_state=new_string
-        self.node.string=self.best_state
-                
-        for i in range(0,len(self.vocabulary)):
-            new_string = self.node.string+ " "+self.vocabulary[i]
-            new_cost = environment.compute_cost(new_string)
-            if(self.node.cost>new_cost):
-                self.node.cost=new_cost
-                self.best_state=new_string
-        self.node.string=self.best_state
-        print("I am out of the 2nd for loop",self.best_state)
+            flag = False
+            trans = []
+            ndp = dp.copy()
+            for i in range(n):
+                if not dp[i]:
+                    if s[i] in transition:
+                        for v in transition[s[i]]:
+                            ns = s[:i] + v + s[i + 1:]
+                            nc = environment.compute_cost(ns)
+                            if (nc < cost):
+                                cost = nc
+                                sentence = ns
+                                flag = True
+                                trans = [i]
+                                self.best_state = sentence
+                    if (i < n - 1) and (not dp[i + 1]):
+                        if s[i:i + 2] in transition:
+                            for v in transition[s[i:i + 2]]:
+                                ns = s[:i] + v + s[i + 2:]
+                                nc = environment.compute_cost(ns)
+                                if (nc < cost):
+                                    cost = nc
+                                    sentence = ns
+                                    flag = True
+                                    trans = [i, i + 1]
+                                    self.best_state = sentence
+                        if (i < n - 2) and (not dp[i + 2]):
+                            if s[i:i + 3] in transition:
+                                for v in transition[s[i:i + 3]]:
+                                    ns = s[:i] + v + s[i + 3:]
+                                    nc = environment.compute_cost(ns)
+                                    if (nc < cost):
+                                        cost = nc
+                                        sentence = ns
+                                        flag = True
+                                        trans = [i, i + 1, i + 2]
+                                        self.best_state = sentence
+
+            if not flag:
+                break
+            l = len(sentence)
+            if (l >= n):
+                for i in trans:
+                    dp[i] = True
+                for i in range(l - n):
+                    dp.insert(trans[0], True)
+                n = l
+                s = sentence
+                self.best_state = s
+            else:
+                for i in trans:
+                    dp[i] = True
+                for i in range(n - l):
+                    del dp[trans[0]]
+                n = l
+                s = sentence
+                self.best_state = s
+
+        bestfront = []
+
+        for i in range(len(vocab)):
+            v = vocab[i]
+            ns = v + ' ' + s
+            nc = environment.compute_cost(ns)
+            bestfront.append((nc, ns))
+            
+        bestfront.sort(key=lambda x: x[0])
+        
+        for i in range(min(3, len(bestfront))):
+            s1 = bestfront[i][1]
+            for i in range(len(vocab)):
+                v = vocab[i]
+                ns = s1 + ' ' + v
+                nc = environment.compute_cost(ns)
+                if (nc < cost):
+                    cost = nc
+                    sentence = ns
+                    self.best_state = sentence
+
+        s = sentence
+        self.best_state = s
+        print(s)
