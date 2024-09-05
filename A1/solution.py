55991d56491c39a38ffdd63bb7915189c2e86fcc
umeshkanala22 <umesh.cse.iitd@gmail.com>
2024-09-05 09:59:57 +0530

    changed driver code to check only 15

--- A1/solution.py
+++ A1/solution.py
@@ -1,25 +1,3 @@
-# class Agent(object):
-#     def __init__(self, phoneme_table, vocabulary) -> None:
-#         """
-#         Your agent initialization goes here. You can also add code but don't remove the existing code.
-#         """
-#         self.phoneme_table = phoneme_table
-#         self.vocabulary = vocabulary
-#         self.best_state = None
-
-#     def asr_corrector(self, environment):
-#         """
-#         Your ASR corrector agent goes here. Environment object has following important members.
-#         - environment.init_state: Initial state of the environment. This is the text that needs to be corrected.
-#         - environment.compute_cost: A cost function that takes a text and returns a cost. E.g., environment.compute_cost("hello") -> 0.5
-
-#         Your agent must update environment.best_state with the corrected text discovered so far.
-#         """
-#         self.best_state = environment.init_state
-#         cost = environment.compute_cost(environment.init_state)
-
-import heapq
-
 class Node(object):
     def __init__(self, string, cost):
         self.string = string
@@ -51,11 +29,8 @@ class Agent(object):
 
 
     def search(self, environment):
-        print(self.phoneme_table)
         isend=False
         start=0
-        print(self.vocabulary)
-
         while isend==False:
             node =self.node
             flag1=False
@@ -86,7 +61,6 @@ class Agent(object):
         for i in range(0,len(self.vocabulary)):
             new_string = self.vocabulary[i] +" "+ self.node.string
             new_cost = environment.compute_cost(new_string)
-            print(new_string)
             if(self.node.cost>new_cost):
                 self.node.cost=new_cost
                 self.best_state=new_string
